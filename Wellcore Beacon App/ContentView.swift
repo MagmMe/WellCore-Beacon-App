4386c0215d336a91fcb1e34754241a7fe15a034a
Marcin Magiera <marcin@magm.me>
2021-10-31 17:42:01 +0100

    Added linear background and home screen text

--- Wellcore Beacon App/ContentView.swift
+++ Wellcore Beacon App/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let gradinetBg = LinearGradient(gradient: Gradient(colors: [
+        Color.purple,
+        Color.red
+    ]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            gradinetBg.ignoresSafeArea()
+            VStack{
+                Text("Wellcore Beacon")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("Just put beacon near to your IPhone.")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Spacer()
+                HStack{
+                }
+            }
+            .foregroundColor(.white)
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
