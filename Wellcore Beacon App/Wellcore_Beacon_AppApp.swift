1799541e623912fe1214122a804fd0436d63b0df
Marcin Magiera <marcin@magm.me>
2021-10-31 13:34:33 +0100

    Initial Commit

---
+++ Wellcore Beacon App/Wellcore_Beacon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wellcore_Beacon_AppApp.swift
+//  Wellcore Beacon App
+//
+//  Created by Magme on 31/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Wellcore_Beacon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
