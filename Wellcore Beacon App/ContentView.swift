1b7fabb8cc72fad4cb183748e58fe400c75397f9
Marcin Magiera <marcin@magm.me>
2021-11-01 19:10:30 +0100

    Created BeaconDetector Class

--- Wellcore Beacon App/ContentView.swift
+++ Wellcore Beacon App/ContentView.swift
@@ -5,7 +5,53 @@
 //  Created by Magme on 31/10/2021.
 //
 
+import Combine
+import CoreLocation
 import SwiftUI
+
+class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate{
+
+    var locationManager: CLLocationManager?
+    @Published var lastDistance = CLProximity.unknown
+    
+    override init(){
+        super.init()
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable(){
+                   startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning(){
+        let uuid = UUID(uuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 0000, minor: 0000)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "My Beacon" )
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if let beacon =  beacons.first{
+            update(distance: beacon.proximity)
+        }else{
+            update(distance: .unknown)
+        }
+    }
+    
+    func update(distance: CLProximity) {
+        lastDistance = distance
+    }
+}
 
 struct ContentView: View {
     
